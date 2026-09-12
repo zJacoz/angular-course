65cc477d157424f9c39e81b92bd3ce7011a3ba97
zJacoz <jaco.net91@gmail.com>
2026-09-11 21:21:54 -0300

    changing app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/pages/auth/auth').then(m => m.Auth)
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./components/pages/home/home').then(m => m.Home)
+  },
+];
---
+++ src/app/components/pages/auth/auth.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  imports: [],
+  templateUrl: './auth.html',
+  styleUrl: './auth.css',
+})
+export class Auth {}
